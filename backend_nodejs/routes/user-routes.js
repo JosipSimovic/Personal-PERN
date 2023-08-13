df692443b3831cd80ea7ffc0e374fc5a8765fe47
Josip Simovic <josip.simovic123@gmail.com>
2023-08-13 13:13:27 +0200

    Commit

--- backend_nodejs/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userController = require("../controllers/user-controllers");
-
-const router = express.Router();
-
-router.post("/signup", userController.signup);
-router.post("/login", userController.login);
-
-module.exports = router;
